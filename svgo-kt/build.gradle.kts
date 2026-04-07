bf41cbb470f1b0d0f8277b6f1b47588b19d5faaf
Rafael Tonholo <rafael@tonholo.dev>
2026-04-07 15:43:11 -0300

    chore: add test infrastructure with kotlin-test and coroutines-test

--- svgo-kt/build.gradle.kts
+++ svgo-kt/build.gradle.kts
@@ -22,6 +22,10 @@ kotlin {
         commonMain.dependencies {
             implementation(libs.kotlinx.coroutines.core)
         }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.kotlinx.coroutines.test)
+        }
     }
 }
 
